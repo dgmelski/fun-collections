5d7f10d3a4c7334d30dd309419a6f7005faa060e
David G Melski <dgmelski@gmail.com>
2022-11-24 12:33:42 +1300

    First attempt at a stack
    Almost no features.

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,60 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
+use std::rc::Rc;
+
+enum List {
+    Cons(i32, Rc<List>),
+    Nil,
+}
+
+use List::{Cons, Nil};
+
+pub struct FunStack {
+    sz: usize,
+    list: Rc<List>,
+}
+
+impl FunStack {
+    pub fn new() -> Self {
+        FunStack {
+            sz: 0,
+            list: Rc::new(Nil),
+        }
+    }
+
+    pub fn clone(&self) -> Self {
+        FunStack {
+            sz: self.sz,
+            list: self.list.clone(),
+        }
+    }
+
+    pub fn push(&mut self, v: i32) -> () {
+        self.list = Rc::new(List::Cons(v, self.list.clone()));
+        self.sz += 1;
+    }
+
+    pub fn top(&self) -> Option<i32> {
+        match *self.list {
+            Cons(v, _) => Some(v),
+            Nil => None,
+        }
+    }
+
+    pub fn pop(&mut self) -> Option<i32> {
+        match self.list.as_ref() {
+            Cons(v, next) => {
+                let ret = Some(*v);
+                self.list = next.clone();
+                self.sz -= 1;
+                ret
+            }
+
+            Nil => None,
+        }
+    }
+
+    pub fn len(&self) -> usize {
+        self.sz
+    }
 }
 
 #[cfg(test)]
@@ -8,7 +63,19 @@ mod tests {
 
     #[test]
     fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+        let mut s = FunStack::new();
+        s.push(1);
+        s.push(2);
+
+        let mut s2 = s.clone();
+        s.push(3);
+        s2.push(4);
+
+        assert_eq!(s.pop(), Some(3));
+        assert_eq!(s.pop(), Some(2));
+        assert_eq!(s.pop(), Some(1));
+        assert_eq!(s2.pop(), Some(4));
+        assert_eq!(s2.pop(), Some(2));
+        assert_eq!(s2.pop(), Some(1));
     }
 }
