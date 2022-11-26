3168daf33a5a7face8cddb110cef4448d5748695
David G Melski <dgmelski@gmail.com>
2022-11-26 23:09:10 +1300

    Add an iterator for FunStack
    As noted in the comments, we opted for the iterator to use references
    and lifetimes.

--- src/lib.rs
+++ src/lib.rs
@@ -13,16 +13,48 @@ pub struct FunStack<T> {
     list: OptList<T>,
 }
 
+// We could implement the iterator to clone the Rc links and the iterated items.
+// This would allow the iterator to outlive the iterated stack or for the stack
+// to be updated during iteration.  However, you can achieve the same effect
+// by iterating a clone of the stack.  We use references because it should be
+// more efficient than cloning Rc's.
+pub struct FunStackIter<'a, T> {
+    next: &'a OptList<T>,
+}
+
+impl<'a, T> Iterator for FunStackIter<'a, T>
+where
+    T: 'a,
+{
+    type Item = &'a T;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        match &self.next {
+            Some(rc) => {
+                let ret = &rc.val;
+                self.next = &rc.next;
+                Some(ret)
+            }
+            None => None,
+        }
+    }
+}
+
+// TODO: Can we get rid of Copy?
 impl<T: Copy> FunStack<T> {
     pub fn new() -> Self {
-        FunStack {
-            sz: 0,
-            list: None,
-        }
+        FunStack { sz: 0, list: None }
+    }
+
+    pub fn iter<'a>(&'a self) -> FunStackIter<'a, T> {
+        FunStackIter { next: &self.list }
     }
 
     pub fn push(&mut self, val: T) -> () {
-        self.list = Some(Rc::new(List { val, next: self.list.take() }));
+        self.list = Some(Rc::new(List {
+            val,
+            next: self.list.take(),
+        }));
         self.sz += 1;
     }
 
@@ -78,5 +110,16 @@ mod tests {
         assert_eq!(s2.pop(), Some(4));
         assert_eq!(s2.pop(), Some(2));
         assert_eq!(s2.pop(), Some(1));
+    }
+
+    #[test]
+    fn fun_stack_iter_test() {
+        let mut s = FunStack::new();
+        s.push('a');
+        s.push('b');
+        s.push('c');
+        for (&c, d) in s.iter().zip(vec!['c', 'b', 'a']) {
+            assert_eq!(c, d);
+        }
     }
 }
